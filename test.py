4c66c1e35714283b656501478a61fc9692db5451
Dmitrii Dryga <Mazixer@yandex.ru>
2025-08-05 09:20:39 +0300

    Добавил изменение в test.py

--- test.py
+++ test.py
@@ -73,3 +73,4 @@ def test_add_two_books(collector):
     collector.add_new_book('Гордость и предубеждение')
     collector.add_new_book('Мастер и Маргарита')
     assert len(collector.get_books_genre()) == 2
+
